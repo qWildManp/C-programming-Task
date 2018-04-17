52b7f57ea281ba15135da09f9cfcd325ab049fa7
HarryLong <qWildManp@users.noreply.github.com>
2018-04-17 20:58:13 +0800

    Other Task

---
+++ Other task/find prime number.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int number,i,n;
+    n=0;
+    printf("Input:\n");
+    scanf_s("%d",&number);
+    if(number==0||number==1){
+        printf("It is not neither a prime number nor have format");
+    }
+    for(i=2;i<number;i++){
+        if(number%i==0) {
+            n++;
+            printf("%d ", i);
+
+        }
+    }
+    if(n==0){
+        printf("It is a prime number");
+        }
+    return 0;
+}
